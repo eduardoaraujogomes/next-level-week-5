61ad093457772a9a3b357124b95ce6d39548207c
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-04-20 12:53:24 -0300

    Refactoring the code

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,7 +5,8 @@ import {
     Image,
     StyleSheet,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    View
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
@@ -16,33 +17,34 @@ import fonts from '../styles/fonts';
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                suas plantas de {'\n'}
+                forma fácil
+                </Text>
 
-            <Image
-                style={styles.image}
-                source={wateringImg}
-                resizeMode="contain"
-            />
-
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.7}
-            >
-                <Feather
-                    name="chevron-right"
-                    style={styles.buttonIcon}
+                <Image
+                    style={styles.image}
+                    source={wateringImg}
+                    resizeMode="contain"
                 />
 
-            </TouchableOpacity>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.7}
+                >
+                    <Feather
+                        name="chevron-right"
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
 
         </SafeAreaView>
     );
@@ -51,17 +53,21 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
         fontFamily: fonts.heading,
-        lineHeight: 38
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
