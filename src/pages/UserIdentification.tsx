5fe4a1a421dab84f2a153bb9bf63db15c673ce60
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-04-23 15:15:47 -0300

    fixed some issues

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -47,12 +47,16 @@ export function UserIdentification() {
 
         try {
             await AsyncStorage.setItem('@plantmanager:user', name);
-            navigation.navigate('Confirmation');
+            navigation.navigate('Confirmation', {
+                title: 'Prontinho',
+                subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+                buttonTitle: 'Começar',
+                icon: 'smile',
+                nextScreen: 'PlantSelect'
+            });
         } catch {
             Alert.alert('Não foi possível salvar o seu nome. 😢');
         }
-
-
     }
 
     return (
